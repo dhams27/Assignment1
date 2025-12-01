5362096cc548a3e35f605a8ac1de42e0f5d06b7b
dhams27 <123472621+dhams27@users.noreply.github.com>
2025-12-01 10:55:30 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,9 @@
+def factorial(num):
+    result = 1
+    for i in range(1, num + 1):
+        result *= i
+    return result
+
+# Call the function and print output
+number = int(input("Enter a number:"))
+print("The factorial of", number, "is:", factorial(number))
---
+++ math module.py
@@ -0,0 +1,10 @@
+import math
+
+num=int(input("Enter a number: "))
+sqrt=math.sqrt(num)
+natural_log=math.log(num)
+sine_value=math.sin(num)
+
+print("Square root of", num, "is:", sqrt)
+print("Natural logarithm of", num, "is:", natural_log)
+print("Sine of", num, "is:", sine_value)
